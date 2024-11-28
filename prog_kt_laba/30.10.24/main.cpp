d5ee28dfc5d18a25345930813ed8768aabfede04
swrneko <sawarinekodesu@gmail.com>
2024-11-28 21:14:58 +0300

    commit

--- prog_kt_laba/30.10.24/main.cpp
+++ prog_kt_laba/30.10.24/main.cpp
@@ -12,36 +12,40 @@
 * Github url    : https://github.com/swrneko/mai_shit.git       *
 * Lit source    :                                               *
 * Created       : 05/11/24                                      *
-* Last Revision : 13/11/24                                      *
+* Last Revision : 27/11/24                                      *
 * Comment(s)    : Сумма ряда.                                   *
 ****************************************************************/
 #include <cmath>
 #include <iostream>
 using namespace std;
 
-float tsum = 0.75; // точная сумма
-int count = 1; // count - кол-во суммированных эл-тов
-int n = 2; // n - порядковый номер элемента
-float sum, eps, diff = 0; // sum - сумма, esp - ввод точности, diff - разность точного значенме и суммы
-
 int main() { // основная функция
+  float tsum;  // точная сумма
+  int n; // порядковый номер элемента
+  float sum; // сумма
+  float eps; // ввод точности
+  float diff; // разность точного значенме и суммы
+
   setlocale(LC_ALL, "Russian");
 
   cout << "Введите точность: ";
   cin >> eps;
-  diff = abs(tsum - sum);
 
   if (eps <= 0) {
     cout << "Введённое число меньше или равно нулю.\n";
     exit(1);
   }
 
+  tsum = 0.75;
+  n = 2;
+  sum = 0;
+  diff = abs(tsum - sum);
+
   while (diff >= eps) {
-    sum = (sum + (1 / (pow(n, 2) - 1)));
+    sum = (sum + (1.0 / ((n - 1)*(n + 1))));
     n += 1;
-    count += 1;
     diff = abs(tsum - sum);
   }
-  cout << "Количество просуммированных элементов: " << count << "\nСумма: " << sum << "\nРазность суммы и точного значения: " << diff << endl;
+  cout << "Количество просуммированных элементов: " << (n - 2) << "\nСумма: " << sum << "\nРазность суммы и точного значения: " << diff << endl;
 }
 
