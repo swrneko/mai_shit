555ab67f41b36e1e46496ba69b01ee76fc2f691f
swrneko <sawarinekodesu@gmail.com>
2024-12-20 21:21:21 +0300

    commit

--- programming_laba/dec_18_2024/main.c
+++ programming_laba/dec_18_2024/main.c
@@ -5,23 +5,6 @@
 
 char AUTHOR_SECOND_NAME[] = "egorov";
 
-void timeMetrics(char * action) {
-  clock_t time_stop;
-  clock_t time_start;
-
-  if (strcmp(action, "start") == 0) {
-    printf("%s", "HELLO");
-    time_start = clock();
-  }
-  if (strcmp(action, "stop") == 0) {
-    time_stop = clock();
-  }
-  if (strcmp(action, "get_time") == 0) {
-     double time_used = ((double)(time_stop - time_start));
-    printf("%f", time_used);
-  }
-}
-
 /* Функция подсчта и вывода степени двойки */
 void countGradeOfTwo(void) {
   unsigned long long two_grade_demical = 1; // Значение степени двойки в 10 системе счисления
@@ -73,7 +56,7 @@ void authorSecondName(void) {
   unsigned long long two_grade_demical_old = 1; // Страрое значение двойки в степени (для вычисления разности) 
 
   /* Печать шапки */
-  printf("%s",
+  printf("\n%s",
     "#########################################################\n"
     "# #######   #######   #### ##    ## ########   #######  #\n"
     "# ##     ## ##     ##  ##  ###   ##    ##     ##     ## #\n"
@@ -87,7 +70,7 @@ void authorSecondName(void) {
   printf("%s", "---------------------------------------------------------\n");
 
   /* Цикл для вывода фамилии автора при отличии значения на 1 000 000 000 */
-  for(int i=63,s; i != -1 ; i += 10) {
+  for(int i=48,s; i != -1 ; i += 10) {
 
     /* Расчёт квадрата двойки */
     two_grade_demical = pow(2, i);
@@ -98,8 +81,8 @@ void authorSecondName(void) {
     }
 
     /* Просчёт разницы старого и нового значения степени двойки т.е. нахождение разницы в 1 000 000 000 с последующей печатью фамилии автора */
-    if ((two_grade_demical - two_grade_demical_old) >= 1000000000) {
-      printf("%s %llu %-16s","|", two_grade_demical, "|");
+    if ((two_grade_demical - two_grade_demical_old) >= 1000000000000) {
+      printf("%s %-19llu %-16s","|", two_grade_demical, "|");
       printf("%s %-15s %s\n", "|", "Egorov", "|");
       two_grade_demical_old = two_grade_demical;
     }
@@ -145,10 +128,9 @@ void bitShiftAction(char * dir_first, char * dir_second, int input_num, char * A
 
 /* Функция вывода побитового сдвига */
 void bitShift(void) {
-  timeMetrics("start");
   int two_grade_32_shift = pow(2, 32);
 
-  printf("%s",
+  printf("\n%s",
   "#########################################################\n"
   "# ########   #######  #### ##    ## ########   #######  #\n"
   "# ##     ## ##     ##  ##  ###   ##    ##     ##     ## #\n"
@@ -157,7 +139,7 @@ void bitShift(void) {
   "# ##        ##     ##  ##  ##  ####    ##            ## #\n"
   "# ##        ##     ##  ##  ##   ###    ##     ##     ## #\n"
   "# ##         #######  #### ##    ##    ##      #######  #\n"
-  "#########################################################\n\n");
+  "#########################################################\n");
 
   /* Вывод двойки в 32 степени (базовый вывод) */
   printf("%s \n\t%s %d\n\n", "Basic output:", "2^32:", two_grade_32_shift);
@@ -167,14 +149,29 @@ void bitShift(void) {
   bitShiftAction("l", "r", two_grade_32_shift, AUTHOR_SECOND_NAME);
   printf("\n%s\n", "Bit shift for second varible.");
   bitShiftAction("r", "l", two_grade_32_shift, AUTHOR_SECOND_NAME);
-
-  timeMetrics("stop");
   }
 
 
 int main() {
+  clock_t clock_time_start;
+  clock_t clock_time_end;
+  time_t real_time_start;
+  time_t real_time_end;
+  double clock_time_used;
+  double real_time_used;
+
+  clock_time_start = clock();
+  time(&real_time_start);
+
   countGradeOfTwo(); // Подсчёт и вывод степени двойки в 2-х системах счисления
   authorSecondName(); // Вывод фамилии автора спустя каждый 1 000 000 000 
   bitShift(); // Расчёт и вывод побитового сдвига влево, затем вправо
-  timeMetrics("get_time");
+
+  clock_time_end = clock(); 
+  time(&real_time_end);
+
+  clock_time_used = ((double) (clock_time_end - clock_time_start)) / CLOCKS_PER_SEC;
+  real_time_used = difftime(real_time_end, real_time_start);
+
+  printf("\nTIME METRICKS:\n\tCPU time used: %0.4f\n\tReal time used:%0.4f\n", clock_time_used, real_time_used);
 }
