67a7601e198807e7ccf0d959eba5c014512bba64
swrneko <sawarinekodesu@gmail.com>
2024-12-24 23:59:07 +0300

    commit

--- proekt_deyatelnost_laba/main.cpp
+++ proekt_deyatelnost_laba/main.cpp
@@ -14,11 +14,8 @@
 * Last Revision : 24/12/24                                                           *
 * Comment(s)    : Метод хорд поиска корней уравнения.                                *
 *************************************************************************************/
-
-
 #include <iostream>
 #include <cmath>
-#include <vector>
 #include <iomanip>
 
 using namespace std;
@@ -31,7 +28,8 @@ int main() {
     int steps = 1000;
 
     // Поиск интервалов изменения знака функции
-    vector<pair<double, double>> intervals;
+    double intervals[1000][2]; // Массив для хранения интервалов
+    int interval_count = 0; // Счетчик интервалов
     double step = (end - start) / steps;
     double x_prev = start;
     double y_prev = pow(x_prev, 3) - 2 * pow(x_prev, 2) - 4 * x_prev + 7;
@@ -40,18 +38,22 @@ int main() {
         double x_curr = start + i * step;
         double y_curr = pow(x_curr, 3) - 2 * pow(x_curr, 2) - 4 * x_curr + 7;
         if (y_prev * y_curr < 0) {
-            intervals.emplace_back(x_prev, x_curr);
+            intervals[interval_count][0] = x_prev; // Начало интервала
+            intervals[interval_count][1] = x_curr; // Конец интервала
+            interval_count++;
         }
         x_prev = x_curr;
         y_prev = y_curr;
     }
 
     cout << "Интервалы, где функция меняет знак:" << endl;
-    for (auto &[a, b] : intervals) {
-        cout << "[" << a << ", " << b << "]" << endl;
+    for (int i = 0; i < interval_count; ++i) {
+        cout << "[" << intervals[i][0] << ", " << intervals[i][1] << "]" << endl;
     }
 
-    for (auto &[a, b] : intervals) {
+    for (int i = 0; i < interval_count; ++i) {
+        double a = intervals[i][0];
+        double b = intervals[i][1];
         cout << "Интервал: " << "[" << a << ", " << b << "]" << endl;
 
         // Метод хорд (секущих)
@@ -61,14 +63,14 @@ int main() {
         int iter_secant = 0;
         double root_secant = NAN;
 
-        for (int i = 0; i < max_iter; ++i) {
+        for (int j = 0; j < max_iter; ++j) {
             double fx0 = pow(x0, 3) - 2 * pow(x0, 2) - 4 * x0 + 7;
             double fx1 = pow(x1, 3) - 2 * pow(x1, 2) - 4 * x1 + 7;
             if (fabs(fx1 - fx0) < 1e-12) break; // предотвращение деления на ноль
             double x2 = x1 - fx1 * (x1 - x0) / (fx1 - fx0);
             if (fabs(pow(x2, 3) - 2 * pow(x2, 2) - 4 * x2 + 7) < tol) {
                 root_secant = x2;
-                iter_secant = i + 1;
+                iter_secant = j + 1;
                 break;
             }
             x0 = x1;
@@ -80,14 +82,14 @@ int main() {
         int iter_newton = 0;
         double root_newton = NAN;
 
-        for (int i = 0; i < max_iter; ++i) {
+        for (int j = 0; j < max_iter; ++j) {
             double fx0 = pow(x_newton, 3) - 2 * pow(x_newton, 2) - 4 * x_newton + 7;
             double dfx0 = 3 * pow(x_newton, 2) - 4 * x_newton - 4;
             if (fabs(dfx0) < 1e-12) break; // предотвращение деления на ноль
             double x1 = x_newton - fx0 / dfx0;
             if (fabs(pow(x1, 3) - 2 * pow(x1, 2) - 4 * x1 + 7) < tol) {
                 root_newton = x1;
-                iter_newton = i + 1;
+                iter_newton = j + 1;
                 break;
             }
             x_newton = x1;
@@ -99,4 +101,3 @@ int main() {
 
     return 0;
 }
-
