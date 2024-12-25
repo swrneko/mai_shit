d6031f33902d539ce1f5fb6150cff35f18368336
swrneko <sawarinekodesu@gmail.com>
2024-12-25 11:18:25 +0300

    commit

--- proekt_deyatelnost_laba/main.cpp
+++ proekt_deyatelnost_laba/main.cpp
@@ -14,77 +14,97 @@
 * Last Revision : 24/12/24                                                           *
 * Comment(s)    : Метод хорд поиска корней уравнения.                                *
 *************************************************************************************/
+
+/************************
+* Подключение библиотек *
+************************/
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-
 using namespace std;
 
+/****************
+* Основное тело *
+****************/
 int main() {
+    /* Установка точности числа после запятой */
     cout << fixed << setprecision(6);
 
-    // Определяем границы поиска и шаги
-    double start = -3.0, end = 3.0;
-    int steps = 1000;
+    /* Определяем границы поиска и шаги */
+    double start = -3.0, end = 3.0; // Начало и конец интервала
+    int steps = 1000; // Количество частей деления интервала
 
-    // Поиск интервалов изменения знака функции
-    double intervals[1000][2]; // Массив для хранения интервалов
+    /* Поиск интервалов изменения знака функции */
+    double intervals[1000][2]; // Массив для хранения интервалов, принимащий в себя 2 значения
     int interval_count = 0; // Счетчик интервалов
-    double step = (end - start) / steps;
-    double x_prev = start;
-    double y_prev = pow(x_prev, 3) - 2 * pow(x_prev, 2) - 4 * x_prev + 7;
+    double step = (end - start) / steps; // Просчёт нового шага
+    double x_prev = start; // Задаём значение предыдущего x
+    double y_prev = pow(x_prev, 3) - 2 * pow(x_prev, 2) - 4 * x_prev + 7; // Задаём значение предыдущего y с учётом ф-ции
 
+    /* Цикл поиска значений, при которых функция меняет знак */
     for (int i = 1; i <= steps; ++i) {
-        double x_curr = start + i * step;
-        double y_curr = pow(x_curr, 3) - 2 * pow(x_curr, 2) - 4 * x_curr + 7;
-        if (y_prev * y_curr < 0) {
+        double x_curr = start + i * step; // Находим значение текущего x
+        double y_curr = pow(x_curr, 3) - 2 * pow(x_curr, 2) - 4 * x_curr + 7; // Находим значение текущего y
+
+        /* Проверка на изменение знака */
+        if (y_prev * y_curr < 0) { 
             intervals[interval_count][0] = x_prev; // Начало интервала
             intervals[interval_count][1] = x_curr; // Конец интервала
-            interval_count++;
+            interval_count++; // Подсчёт количества интервалов
         }
-        x_prev = x_curr;
-        y_prev = y_curr;
+        x_prev = x_curr; // Перезапись x
+        y_prev = y_curr; // Перезапись y
     }
 
+    /* Вывод интервалов смены знака */
     cout << "Интервалы, где функция меняет знак:" << endl;
     for (int i = 0; i < interval_count; ++i) {
         cout << "[" << intervals[i][0] << ", " << intervals[i][1] << "]" << endl;
     }
 
+    /* Ввод точности определения значений */
     double tol;
     cout << "Введите точность: ";
     cin >> tol;
 
+    /* Проверка ввода точности на > 1 */
     if(tol > 1) {
         cout << "Ошибка: Точность должна быть < 1.\n";
         exit(1);
     }
+
+    /* Проверка ввода точности на < 0 */
     if(tol < 0) {
         cout << "Ошибка: Точность должна быть > 0.\n";
         exit(1);
     } 
+
+    /* Проверка ввода точности на значение отличное от числа */
     if(int(tol) == false) {
         cout << "Ошибка: Введённое значение не является числом.\n";
         exit(1);
     } 
 
+    /* Вывод знчений */
     for (int i = 0; i < interval_count; ++i) {
-        double a = intervals[i][0];
-        double b = intervals[i][1];
-        cout << "Интервал: " << "[" << a << ", " << b << "]" << endl;
+        double a = intervals[i][0]; // Перенос первого интервала в переменную
+        double b = intervals[i][1]; // Перенос второго интервала в переменную
+        cout << "Интервал: " << "[" << a << ", " << b << "]" << endl; // Вывод интервалов
 
         // Метод хорд (секущих)
-        double x0 = a, x1 = b; 
-        int max_iter = 100;
-        int iter_secant = 0;
+        double x0 = a, x1 = b;  // Определение начальных значений ф-ций
+        int max_iter = 100; // Максимальное число итераций
+        int iter_secant = 0; // Переменная подсчёта количество итераций метода секущих
         double root_secant = NAN;
 
+        /* Подсчёт значения функции по методу секущих */
         for (int j = 0; j < max_iter; ++j) {
-            double fx0 = pow(x0, 3) - 2 * pow(x0, 2) - 4 * x0 + 7;
-            double fx1 = pow(x1, 3) - 2 * pow(x1, 2) - 4 * x1 + 7;
+            double fx0 = pow(x0, 3) - 2 * pow(x0, 2) - 4 * x0 + 7; // Значение функции по x0
+            double fx1 = pow(x1, 3) - 2 * pow(x1, 2) - 4 * x1 + 7; // Значение функции по x1
             if (fabs(fx1 - fx0) < 1e-12) break; // предотвращение деления на ноль
-            double x2 = x1 - fx1 * (x1 - x0) / (fx1 - fx0);
-            if (fabs(pow(x2, 3) - 2 * pow(x2, 2) - 4 * x2 + 7) < tol) {
+            double x2 = x1 - fx1 * (x1 - x0) / (fx1 - fx0); // Нахождения x2
+            /*  */
+            if (fabs(pow(x2, 3) - 2 * pow(x2, 2) - 4 * x2 + 7) < tol) { 
                 root_secant = x2;
                 iter_secant = j + 1;
                 break;
