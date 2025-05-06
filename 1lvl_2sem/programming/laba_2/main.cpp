78e4423892c12061c779bba3440781df08c08aa3
swrneko <sawarinekodesu@gmail.com>
2025-05-06 11:26:20 +0300

    clean some staff

--- 1lvl_2sem/programming/laba_2/main.cpp
+++ 1lvl_2sem/programming/laba_2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< HEAD
 using namespace std;
 
 const int ROWS = 3;
@@ -25,3 +26,42 @@ int main() {
     return 0;
 }
 
+=======
+
+using namespace std;
+
+
+void print_matrix(int matrix_size, int** matrix) {
+  for(int i=0; i < matrix_size; i++) {
+      for(int j=0; j < matrix_size; j++) {
+        cout << matrix[i][j] << " ";
+      }
+      cout << endl;
+    }
+}
+
+void input_matrix() {
+  int matrix_size = 0; 
+  cout << "Введите размерность матрицы M(A;A): " << endl;
+  cin >> matrix_size;
+
+  int matrix[matrix_size][matrix_size];
+
+  int** matrix = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        matrix[i] = new int[size];
+    }
+
+  cout << "Введите матрицу: " << endl;
+  for(int i=0; i < matrix_size; i++) {
+    for(int j=0; j < matrix_size; j++) {
+      cin >> matrix[i][j];
+    }
+  }
+}
+
+int main() {
+  input_matrix();
+  return 0;
+}
+>>>>>>> afb4634 (add laba 2 (programming))
