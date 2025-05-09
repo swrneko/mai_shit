b88057edb2ac2db0a9b3d91df537a976ac2f0418
swrneko <sawarinekodesu@gmail.com>
2025-05-09 21:32:42 +0300

    end work on laba_3

--- 1lvl_2sem/programming/laba_2/main.cpp
+++ 1lvl_2sem/programming/laba_2/main.cpp
@@ -1,67 +1,45 @@
 #include <iostream>
-<<<<<<< HEAD
 using namespace std;
 
-const int ROWS = 3;
-const int COLS = 3;
-
-int main() {
-    int matrix[ROWS][COLS];
-
-    // Ввод матрицы
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-
-    // Вывод матрицы
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS; ++j) {
+void print_matrix(int** matrix, int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
-
-    return 0;
-}
-
-=======
-
-using namespace std;
-
-
-void print_matrix(int matrix_size, int** matrix) {
-  for(int i=0; i < matrix_size; i++) {
-      for(int j=0; j < matrix_size; j++) {
-        cout << matrix[i][j] << " ";
-      }
-      cout << endl;
-    }
 }
 
 void input_matrix() {
-  int matrix_size = 0; 
-  cout << "Введите размерность матрицы M(A;A): " << endl;
-  cin >> matrix_size;
+    int size;
+    cout << "Введите размерность матрицы M(A;A): ";
+    cin >> size;
 
-  int matrix[matrix_size][matrix_size];
-
-  int** matrix = new int*[size];
+    // Выделение памяти
+    int** matrix = new int*[size];
     for (int i = 0; i < size; ++i) {
         matrix[i] = new int[size];
     }
 
-  cout << "Введите матрицу: " << endl;
-  for(int i=0; i < matrix_size; i++) {
-    for(int j=0; j < matrix_size; j++) {
-      cin >> matrix[i][j];
+    cout << "Введите матрицу:" << endl;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            cin >> matrix[i][j];
+        }
     }
-  }
+
+    cout << "Матрица:" << endl;
+    print_matrix(matrix, size);
+
+    // Очистка памяти
+    for (int i = 0; i < size; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
 }
 
 int main() {
-  input_matrix();
-  return 0;
+    input_matrix();
+    return 0;
 }
->>>>>>> afb4634 (add laba 2 (programming))
+
